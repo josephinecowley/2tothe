880e7e8402761be545c50f2f858be892ec4614de
Patrick Pan <patrick.pan@patrickpan.com>
2025-01-17 21:43:50 -0800

    add eslint

---
+++ backend/eslint.config.mjs
@@ -0,0 +1,15 @@
+import globals from "globals";
+import pluginJs from "@eslint/js";
+import tseslint from "typescript-eslint";
+import tsParser from "@typescript-eslint/parser";
+
+/** @type {import('eslint').Linter.Config[]} */
+export default [
+  { ignores: ["dist", "eslint.config.mjs"] },
+  { files: ["**/*.{js,mjs,cjs,ts}"] },
+  { languageOptions: { globals: globals.node, parser: tsParser, parserOptions: { project: "./tsconfig.json" } } },
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+  ...tseslint.configs.strictTypeChecked,
+  ...tseslint.configs.stylistic,
+];
