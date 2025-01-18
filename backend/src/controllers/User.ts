760d6b27d34ba9a4424677972964d21c2d119b99
josephinecowley <josephinecowley1010@gmail.com>
2025-01-18 14:31:26 -0800

    add function for set user nickname in user controller

--- backend/src/controllers/User.ts
+++ backend/src/controllers/User.ts
@@ -1,19 +1,16 @@
 import { ErrorCode } from "@2tothe/shared";
 import { Place, User } from "../db/entities";
-export async function findUserByNickname(nickname: string) {
+
+async function findUserByNickname(nickname: string) {
   return await User.findOne({ where: { nickname }, relations: { place: true } });
 }
 
-export async function stateUserPlace(nickname: User["nickname"]) {
-  const user = await findUserByNickname(nickname);
-  if (!user) {
-    return `No one is called ${nickname}.`;
-  }
-  return `${nickname} is from ${user.place.placeName}.`;
+async function findUserByID(userID: User["id"]) {
+  return await User.findOne({ where: { id: userID }, relations: { place: true } });
 }
 
 export async function setUserPlace(userID: User["id"], newPlaceID: Place["id"]) {
-  const user = await User.findOne({ where: { id: userID }, relations: { place: true } });
+  const user = await findUserByID(userID);
   if (!user) {
     return {
       errorCode: ErrorCode.NotFound,
@@ -33,6 +30,23 @@ export async function setUserPlace(userID: User["id"], newPlaceID: Place["id"]) 
   await user.save();
 
   return {
-    message: `User place was updated to ${newPlace.placeName}`,
+    message: `User place was updated to ${user.place.placeName}`,
+  };
+}
+
+export async function setUserNickname(userID: User["id"], newNickname: User["nickname"]) {
+  const user = await findUserByID(userID);
+  if (!user) {
+    return {
+      errorCode: ErrorCode.NotFound,
+      message: `No user has the id: ${userID}`,
+    };
+  }
+
+  user.nickname = newNickname;
+  await user.save();
+
+  return {
+    message: `User place was updated to ${user.nickname}`,
   };
 }
