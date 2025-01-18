0690234a41e33c729f859a7d2b3648f9b203cf67
Patrick Pan <patrick.pan@patrickpan.com>
2025-01-18 00:49:23 -0800

    change GET to POST to allow body

--- shared/src/routes/HelloWorld.ts
+++ shared/src/routes/HelloWorld.ts
@@ -3,7 +3,7 @@ import { Route } from "../routing";
 
 export const helloWorldRoute: Route<{ name: string }, { message: string }> = {
   path: "/hello-world",
-  method: "get",
+  method: "post",
   req: z.object({ name: z.string() }),
   res: z.object({ message: z.string() }),
 };
