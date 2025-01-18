3641ea42ecebcc072bf85ded4966875b83c53090
josephinecowley <josephinecowley1010@gmail.com>
2025-01-18 14:41:05 -0800

    set up route for set user nickname

--- backend/src/routes/UserSettings.ts
+++ backend/src/routes/UserSettings.ts
@@ -1,12 +1,20 @@
 import { Router } from "express";
 import { applyRoute } from "./applyRoute";
 import { ErrorCode, ResponseError, Routes } from "@2tothe/shared";
-import { setUserPlace } from "../controllers/User";
+import { setUserPlace, setUserNickname } from "../controllers/User";
 
 const router = Router();
 
 applyRoute(router, Routes.UserSettings.setUserPlaceRoute).use(async (body) => {
   const result = await setUserPlace(body.userID, body.newPlaceID);
+  if (result.errorCode) {
+    throw new ResponseError(404, result.errorCode, result.message);
+  }
+  return { success: true };
+});
+
+applyRoute(router, Routes.UserSettings.setUserNicknameRoute).use(async (body) => {
+  const result = await setUserNickname(body.userID, body.newNickname);
   if (result.errorCode) {
     throw new ResponseError(404, result.errorCode, result.message);
   }
