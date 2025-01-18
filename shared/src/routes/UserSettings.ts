0516dea79af6ca01a8d2f8d8420c5b050b5a9a7a
josephinecowley <josephinecowley1010@gmail.com>
2025-01-18 14:01:24 -0800

    prepend /user-settings to path

--- shared/src/routes/UserSettings.ts
+++ shared/src/routes/UserSettings.ts
@@ -2,7 +2,7 @@ import { z } from "zod";
 import { Route } from "../routing";
 
 export const setUserPlaceRoute: Route<{ userID: string; newPlaceID: string }, { success: boolean }> = {
-  path: "/set-user-place",
+  path: "/user-settings/set-user-place",
   method: "post",
   req: z.object({ userID: z.string().uuid(), newPlaceID: z.string().uuid() }),
   res: z.object({ success: z.boolean() }),
