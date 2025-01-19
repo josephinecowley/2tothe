cbd6ee836ff41978ff2196d324ecba9a633a25be
josephinecowley <josephinecowley1010@gmail.com>
2025-01-19 11:58:25 -0800

    updated name of question schedule to ScheduledQuestion

--- backend/src/db/entities/ScheduledQuestion.ts
+++ backend/src/db/entities/ScheduledQuestion.ts
@@ -2,7 +2,7 @@ import { BaseEntity, Column, Entity, ManyToOne, PrimaryGeneratedColumn } from "t
 import { Question } from "./Question";
 
 @Entity()
-export class QuestionSchedule extends BaseEntity {
+export class ScheduledQuestion extends BaseEntity {
   @PrimaryGeneratedColumn("uuid")
   id!: string;
 
--- backend/src/db/entities/index.ts
+++ backend/src/db/entities/index.ts
@@ -3,4 +3,4 @@ export { User } from "./User";
 export { NewUser } from "./NewUser";
 export { Question } from "./Question";
 export { UserAnswer } from "./UserAnswer";
-export { QuestionSchedule } from "./QuestionSchedule";
+export { ScheduledQuestion } from "./ScheduledQuestion";
