4ac77e62d440dc0d1ae010f07d33d0067ecca83c
josephinecowley <josephinecowley1010@gmail.com>
2025-01-19 16:23:58 -0800

    create getSomeAnswerableQuestionsForUser

--- backend/src/controllers/Question.ts
+++ backend/src/controllers/Question.ts
@@ -1,5 +1,5 @@
 import { ErrorCode } from "@2tothe/shared";
-import { Place, ScheduledQuestion, Question } from "../db/entities";
+import { Place, ScheduledQuestion, Question, User } from "../db/entities";
 
 export async function getMostRecentQuestionForPlace(placeID: Place["id"]) {
   const now = new Date();
@@ -12,9 +12,40 @@ export async function getMostRecentQuestionForPlace(placeID: Place["id"]) {
   if (!mostRecentScheduledQuestion) {
     return {
       errorCode: ErrorCode.NotFound,
-      message: `No past questions found for place ${placeID}`,
+      message: `No past questions found for place ${placeID}.`,
     };
   }
 
   return { mostRecentQuestionID: mostRecentScheduledQuestion.questionID };
+}
+
+export async function getSomeAnswerableQuestionsForUser(userID: User["id"], numberOfQuestions: number) {
+  const now = new Date();
+  const user = await User.findOne({ where: { id: userID }, relations: { place: true } });
+  if (!user) {
+    return {
+      errorCode: ErrorCode.NotFound,
+      message: `No user found with id: ${userID}.`,
+    };
+  }
+
+  const placeID = user.place.id;
+
+  const answerableQuestions = await Question.createQueryBuilder("question")
+    // Find all questions which have not been answered by user
+    .leftJoinAndSelect("question.userAnswers", "ua_for_user", "ua_for_user.userID = :userID", { userID })
+    .leftJoinAndSelect("ua_for_user.user", "user")
+    .where("ua_for_user.id IS NULL")
+
+    // Filter for questions in the past of the user's place
+    .leftJoinAndSelect("question.scheduledQuestions", "sq", "sq.placeID = :placeID", { placeID })
+    .andWhere("sq.dateTime <= :now", { now })
+
+    // Filter also for timeless
+    .andWhere("question.timeless = true")
+
+    .take(numberOfQuestions)
+    .getMany();
+
+  return answerableQuestions;
 }
